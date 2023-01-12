a7549564095722938ab10a5018ce838092968c9f
Samuel Adekunle <me@samuel-adekunle.software>
2023-01-12 10:29:40 +0000

    completed two way message passing

--- link.py
+++ link.py
@@ -1,7 +1,8 @@
 import simpy
 import random
+from common import *
 
-
+# Perfect PointToPoint Link
 class PointToPointLink(object):
   def __init__(self, env, capacity = float('inf'), min_delay=0, max_delay = 0):
     self.env = env
@@ -10,29 +11,45 @@ class PointToPointLink(object):
     self.max_delay = max_delay
     self.store = simpy.Store(self.env, capacity=capacity)
 
-  def send(self, msg):
-    self.env.process(self.latency(msg))
+  def send(self, event):
+    self.env.process(self.latency(event))
 
-  def latency(self, msg):
+  def latency(self, event):
     yield self.env.timeout(random.randint(self.min_delay, self.max_delay))
-    self.store.put(msg)
+    self.store.put(event)
 
   def receive(self):
     return self.store.get()
 
+# Best Effort Broadcast Link
 class BroadcastLink(object):
-  def __init__(self, env, capacity = float('inf'), min_delay=0, max_delay = 0):
+  def __init__(self, env, capacity = float('inf'), min_delay=0, max_delay = 0, timeout=1):
     self.env = env
     self.capacity = capacity
     self.min_delay = min_delay
     self.max_delay = max_delay
-    self.links = []
 
-  def get_new_link(self):
-    link = PointToPointLink(self.env, self.capacity, self.min_delay, self.max_delay)
-    self.links.append(link)
-    return link
+    self.in_link = self._new_link()
+    self.out_links = {}
 
-  def send(self, msg):
-    for link in self.links:
-      link.send(msg)
+  def send(self, event):
+    self.in_link.send(event)
+
+  def get_new_out_link(self, name):
+    new_link = self._new_link()
+    self.out_links[name] = new_link
+    return new_link
+
+  def run(self):
+    while True:
+        event = yield self.in_link.receive()
+        if event.name == "broadcast_send":
+          for link in self.out_links.values():
+            link.send(event)
+        elif event.name == "broadcast_deliver":
+          self.out_links[event.attributes["receiver"]].send(event)
+        else:
+          raise Exception("Unknown event name: %s" % event.name)
+
+  def _new_link(self):
+    return PointToPointLink(self.env, self.capacity, self.min_delay, self.max_delay)
--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 import simpy
 import random
 from link import *
+
 RANDOM_SEED = 42
 
-env = simpy.Environment()
-random.seed(RANDOM_SEED)
-
-bc_link = BroadcastLink(env, capacity=1, min_delay=1, max_delay=10)
-
-link1 = bc_link.get_new_link()
-link2 = bc_link.get_new_link()
-
-def sender():
+def sender(name, link):
+  counter = 0
   while True:
-    yield env.timeout(random.randint(1,5))
-    msg = "Message sent at time %d" % env.now
-    print(msg)
-    bc_link.send(msg)
+    with link.receive() as req:
+      result = yield req | env.timeout(random.randint(1,5))
+      if req not in result:
+        event = Event("broadcast_send", {"sender": name, "message": "Hello World %d" % counter, "time": env.now})
+        counter += 1
+        bc_link.send(event)
+        continue
+      event = result[req]
+      print("%s Received event '%s' at time %d" % (name, event, env.now))
+      if event.name == "broadcast_send":
+        bc_link.send(Event("broadcast_deliver", {"sender": name, "receiver": event.attributes["sender"], "message": event.attributes["message"], "time": env.now}))
+      yield env.timeout(random.randint(1,5))
 
 def receiver(name, link):
   while True:
-    msg = yield link.receive()
-    print("%s Received message '%s' at time %d" % (name, msg, env.now))
+    event = yield link.receive()
+    print("%s Received event '%s' at time %d" % (name, event, env.now))
+    bc_link.send(Event("broadcast_deliver", {"sender": name, "receiver": event.attributes["sender"], "message": event.attributes["message"], "time": env.now}))
     yield env.timeout(random.randint(1,5))
 
-env.process(sender())
-env.process(receiver("Receiver 1", link1))
-env.process(receiver("Receiver 2", link2))
+
+env = simpy.Environment()
+random.seed(RANDOM_SEED)
+bc_link = BroadcastLink(env, capacity=1, min_delay=1, max_delay=10, timeout=5)
+link1 = bc_link.get_new_out_link("Sender")
+link2 = bc_link.get_new_out_link("Receiver 1")
+link3 = bc_link.get_new_out_link("Receiver 2")
+env.process(sender("Sender", link1))
+env.process(receiver("Receiver 1", link2))
+env.process(receiver("Receiver 2", link3))
+env.process(bc_link.run())
 env.run(until=40)
